8ea4950f3af49bf0efe015952378dff60c5b18ed
Kaplc <755536175@qq.com>
2023-05-13 00:43:01 +0800

    初始化控制台

--- project/Rescue Princess/Program.cs
+++ project/Rescue Princess/Program.cs
@@ -6,7 +6,21 @@ namespace Rescue_Princess
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("hello world!");
+            #region 初始化控制台
+            // 缓冲区大小
+            Console.BufferHeight = 30;
+            Console.BufferWidth = 150;
+            // 控制台窗口
+            Console.WindowHeight = 30;
+            Console.WindowWidth = 50;
+            // 隐藏光标
+            Console.CursorVisible = false;
+    
+            #endregion
+
+            Console.ReadLine();
+            
+
         }
     }
-}
+}  
