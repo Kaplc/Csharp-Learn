d7e7298db6b2f9c6a5cc956224de252ef6de6662
Kaplc <755536175@qq.com>
2023-05-13 01:01:55 +0800

    游戏主循环进入场景函数

--- project/Rescue Princess/Program.cs
+++ project/Rescue Princess/Program.cs
@@ -4,9 +4,22 @@ namespace Rescue_Princess
 {
     internal class Program
     {
+        static void start_scene()
+        {
+        }
+
+        static void game_scene()
+        {
+        }
+
+        static void end_scene()
+        {
+        }
+
         public static void Main(string[] args)
         {
             #region 初始化控制台
+
             // 缓冲区大小
             Console.BufferHeight = 30;
             Console.BufferWidth = 150;
@@ -15,12 +28,28 @@ namespace Rescue_Princess
             Console.WindowWidth = 50;
             // 隐藏光标
             Console.CursorVisible = false;
-    
+
             #endregion
 
-            Console.ReadLine();
-            
+            int scene_num = 1;
+            // 游戏主循环
+            while (true)
+            {
+                switch (scene_num)
+                {
+                    case 1:
+                        start_scene();
+                        break;
+                    case 2:
+                        game_scene();
+                        break;
+                    case 3:
+                        end_scene();
+                        break;
+                }
+            }
 
+            Console.ReadLine();
         }
     }
-}  
+}
