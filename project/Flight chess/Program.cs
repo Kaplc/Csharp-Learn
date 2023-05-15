4247cec5d8e172d97e6468975d9e3168eb454fda
Kaplc <755536175@qq.com>
2023-05-15 23:57:39 +0800

    更新:完成初始化控制台函数

--- project/Flight chess/Program.cs
+++ project/Flight chess/Program.cs
@@ -1,9 +1,25 @@
-﻿namespace Flight_chess
+﻿using System;
+
+namespace Flight_chess
 {
+    
     internal class Program
     {
+        public static void init()
+        {
+            int windowsWilde = 60;
+            int windowsHight = 30;
+            Console.WindowWidth = windowsWilde;
+            Console.WindowHeight = windowsHight;
+            Console.BufferWidth = windowsWilde;
+            Console.BufferHeight = windowsHight;
+            Console.CursorVisible = false;
+        }
         public static void Main(string[] args)
         {
+            init();
+            Console.WriteLine("按下任意键继续");
+            Console.ReadLine();
         }
     }
 }
