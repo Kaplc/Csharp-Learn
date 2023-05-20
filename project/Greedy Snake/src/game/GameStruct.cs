7430dfe592274d171f60a3b554d0d13373d513af
Kaplc <755536175@qq.com>
2023-05-20 21:05:39 +0800

    更新:定义坐标结构体并重载运算符

--- project/Greedy Snake/src/game/GameStruct.cs
+++ project/Greedy Snake/src/game/GameStruct.cs
@@ -1,7 +1,23 @@
 namespace Greedy_Snake.game
 {
-    public struct GameStruct
+    public struct Position
     {
-        
+        private int x;
+        private int y;
+
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static bool operator ==(Position p1, Position p2)
+        {
+            return (p1.x == p2.x && p1.y == p2.y);
+        }
+        public static bool operator !=(Position p1, Position p2)
+        {
+            return (p1.x == p2.x && p1.y == p2.y);
+        }
     }
 }
