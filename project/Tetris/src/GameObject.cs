23d8ae06afbc1f58318fcdeff0c758715875fb4f
Kaplc <755536175@qq.com>
2023-05-23 23:10:48 +0800

    更新:位置结构体和方块类型枚举

--- project/Tetris/src/GameObject.cs
+++ project/Tetris/src/GameObject.cs
@@ -1,7 +1,76 @@
 namespace Tetris
 {
+    #region 枚举
+
+    public enum BlockType
+    {
+        Wall,
+        /// <summary>
+        /// 方形
+        /// </summary>
+        Square,
+        /// <summary>
+        /// 长条
+        /// </summary>
+        Strip,
+        /// <summary>
+        /// 坦克
+        /// </summary>
+        Tanker,
+        /// <summary>
+        /// 楼梯
+        /// </summary>
+        Stairs,
+        /// <summary>
+        /// 长拐
+        /// </summary>
+        LongCrutch,
+    }
+
+    #endregion
+
+    #region 结构体
+
+    public struct Position
+    {
+        private int x;
+        private int y;
+
+        public int X
+        {
+            get => x;
+        }
+
+        public int Y
+        {
+            get => y;
+        }
+
+        public Position(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public static bool operator ==(Position p1, Position p2)
+        {
+            return p1.X == p2.X && p1.Y == p2.Y;
+        }
+
+        public static bool operator !=(Position p1, Position p2)
+        {
+            return p1.X != p2.X || p1.Y != p2.Y;
+        }
+
+        public static Position operator +(Position p1, Position p2)
+        {
+            return new Position(p1.X + p2.X, p2.Y + p2.Y);
+        }
+    }
+
+    #endregion
+
     public class GameObject
     {
-        
     }
 }
